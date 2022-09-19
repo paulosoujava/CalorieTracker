e2e445e5bde9878e810c87a548705e16ee3c3f7b
paulosoujava <paulosoujava@gmail.com>
2022-09-19 17:24:54 -0300

    welcome screen

--- app/src/main/java/com/jorge/paulo/calorytracker/MainActivity.kt
+++ app/src/main/java/com/jorge/paulo/calorytracker/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.jorge.paulo.calorytracker.ui.theme.CaloryTrackerTheme
+import com.jorge.paulo.onboarding_presentation.welcome.WelcomeScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,22 +23,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    WelcomeScreen()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    CaloryTrackerTheme {
-        Greeting("Android")
     }
 }
--- app/src/main/java/com/jorge/paulo/calorytracker/ui/theme/Theme.kt
+++ app/src/main/java/com/jorge/paulo/calorytracker/ui/theme/Theme.kt
@@ -1,10 +1,14 @@
 package com.jorge.paulo.calorytracker.ui.theme
+
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.graphics.Color
+import com.jorge.paulo.core_ui.Dimensions
+import com.jorge.paulo.core_ui.LocalSpacing
 
 
 private val DarkColorPalette = darkColors(
@@ -32,16 +36,22 @@ private val LightColorPalette = lightColors(
 )
 
 @Composable
-fun CaloryTrackerTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable() () -> Unit) {
+fun CaloryTrackerTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable() () -> Unit
+) {
     val colors = if (darkTheme) {
         DarkColorPalette
     } else {
         LightColorPalette
     }
-    MaterialTheme(
-        colors = colors,
-        typography = Typography,
-        shapes = Shapes,
-        content = content
-    )
+
+    CompositionLocalProvider(LocalSpacing provides Dimensions()) {
+        MaterialTheme(
+            colors = colors,
+            typography = Typography,
+            shapes = Shapes,
+            content = content
+        )
+    }
 }
